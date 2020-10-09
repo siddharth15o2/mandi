1d9710341af2843a47a3d266d14b0c04d557e83e
Siddharth Singh <07siddharthsingh@gmail.com>
2020-10-09 22:55:37 +0530

    seed districts according states

--- seed-db.js
+++ seed-db.js
@@ -5,21 +5,33 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'world'
+    database: 'world',
+    multipleStatements: true
 });
 
 connection.connect();
 
-let values = [];
+let state_values = [];
 
 for (let i = 0; i < data.states.length; i++) {
-    const { type, name, capital, code } = data.states[i];
-    values.push(`('${type}', '${name}',  '${capital}', '${code}', 1)`);
+    const {id, type, name, capital, code } = data.states[i];
+    state_values.push(`(${id}, '${type}', '${name}',  '${capital}', '${code}', 1)`);
 }
 
-const query = 'insert into states (type, name,  capital, code, country_id) values ' + values.join(',');
+const state_query = 'truncate states; insert into states (id, type, name,  capital, code, country_id) values ' + state_values.join(',');
 
-connection.query(query, function (error, results) {
+let district_values = [];
+
+for (let i = 0; i < data.states.length; i++) {
+    const state_id = data.states[i].id ;
+    for (let j = 0; j < data.states[i].districts.length; j++) {
+        const {name} = data.states[i].districts[j];
+        district_values.push(`('${name}', ${state_id})`);
+    }   
+}
+
+const district_query = 'truncate districts; insert into districts (name, state_id) values' + district_values.join(',');
+connection.query('SET FOREIGN_KEY_CHECKS=0;' + state_query + ';' + district_query + '; SET FOREIGN_KEY_CHECKS=1;' , function (error, results) {
     if (error) throw error;
     console.log(results)
 });
