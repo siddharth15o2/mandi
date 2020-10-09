6dd5c9181781f966643c744c80e599de562e8914
Siddharth Singh <07siddharthsingh@gmail.com>
2020-10-09 23:36:26 +0530

    Basic express setup

--- seed-db.js
+++ seed-db.js
@@ -1,19 +1,19 @@
-const data = require('./data.json');
+const data = require("./data.json");
 
-const mysql = require('mysql');
+const mysql = require("mysql");
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'world',
-    multipleStatements: true
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "world",
+  multipleStatements: true,
 });
 
 const sqlQueries = [
-    'SET FOREIGN_KEY_CHECKS=0',
-    'truncate states',
-    'truncate districts',
-    'SET FOREIGN_KEY_CHECKS=1'
+  "SET FOREIGN_KEY_CHECKS=0",
+  "truncate states",
+  "truncate districts",
+  "SET FOREIGN_KEY_CHECKS=1",
 ];
 
 connection.connect();
@@ -21,25 +21,32 @@ connection.connect();
 let state_values = [];
 
 for (const state of data.states) {
-    const {id, type, name, capital, code } = state;
-    state_values.push(`(${id}, '${type}', '${name}',  '${capital}', '${code}', 1)`);
+  const { id, type, name, capital, code } = state;
+  state_values.push(
+    `(${id}, '${type}', '${name}',  '${capital}', '${code}', 1)`
+  );
 }
 
-sqlQueries.push('insert into states (id, type, name,  capital, code, country_id) values ' + state_values.join(','));
+sqlQueries.push(
+  "insert into states (id, type, name,  capital, code, country_id) values " +
+    state_values.join(",")
+);
 
 let district_values = [];
 
 for (const state of data.states) {
-    for (const district of state.districts) {
-        const {name} = district;
-        district_values.push(`('${name}', ${state.id})`);
-    }
+  for (const district of state.districts) {
+    const { name } = district;
+    district_values.push(`('${name}', ${state.id})`);
+  }
 }
 
-sqlQueries.push('insert into districts (name, state_id) values' + district_values.join(','));
-connection.query(sqlQueries.join('; '), function (error, results) {
-    if (error) throw error;
-    console.log(results)
+sqlQueries.push(
+  "insert into districts (name, state_id) values" + district_values.join(",")
+);
+connection.query(sqlQueries.join("; "), function (error, results) {
+  if (error) throw error;
+  console.log(results);
 });
 
 connection.end();
